92c112e3b5661dcf4b2d106c323eba5edae856c6
Laby Damaro Camara <ldamaro98@gmail.com>
2021-08-08 21:52:43 +0200

    les énumérations en typescript

---
+++ enum.js
@@ -0,0 +1,9 @@
+var Bousole;
+(function (Bousole) {
+    Bousole[Bousole["NORD"] = 0] = "NORD";
+    Bousole[Bousole["SUD"] = 1] = "SUD";
+    Bousole[Bousole["EST"] = 2] = "EST";
+    Bousole[Bousole["OUEST"] = 3] = "OUEST";
+})(Bousole || (Bousole = {}));
+var n = Bousole.NORD;
+console.log(n);
---
+++ enum.ts
@@ -0,0 +1,8 @@
+enum Bousole {
+    NORD,
+    SUD,
+    EST,
+    OUEST
+}
+let n = Bousole.NORD;
+console.log(n)
